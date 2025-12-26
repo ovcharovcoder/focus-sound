9dcaab41f79d318b328ed3871311821cbff83913
Andriy Ovcharov <ovcharovcoder@gmail.com>
2025-12-26 19:06:19 +0200

    Update extension.ts

--- src/extension.ts
+++ src/extension.ts
@@ -51,21 +51,6 @@ function getHtml(webview: vscode.Webview, extensionUri: vscode.Uri): string {
      <option value="Relax_1.mp3">Relax 1</option>
      <option value="Relax_2.mp3">Relax 2</option>
      <option value="Relax_3.mp3">Relax 3</option>
-     <option value="Relax_4.mp3">Relax 4</option>
-     <option value="Relax_5.mp3">Relax 5</option>
-     <option value="Relax_6.mp3">Relax 6</option>
-     <option value="Relax_7.mp3">Relax 7</option>
-     <option value="Relax_8.mp3">Relax 8</option>
-     <option value="Relax_9.mp3">Relax 9</option>
-     <option value="Relax_10.mp3">Relax 10</option>
-     <option value="Relax_11.mp3">Relax 11</option>
-     <option value="Relax_12.mp3">Relax 12</option>
-     <option value="Relax_13.mp3">Relax 13</option>
-     <option value="Relax_14.mp3">Relax 14</option>
-     <option value="Relax_15.mp3">Relax 15</option>
-     <option value="Relax_16.mp3">Relax 16</option>
-     <option value="Relax_17.mp3">Relax 17</option>
-     <option value="Relax_18.mp3">Relax 18</option>
     </select>
 
     <button id="play">Play</button>
@@ -83,3 +68,4 @@ function getHtml(webview: vscode.Webview, extensionUri: vscode.Uri): string {
 }
 
 export function deactivate() {}
+
